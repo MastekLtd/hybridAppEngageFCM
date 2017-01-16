c690684b5a40d551075c10104b76a06b3f5ee8cd
Pravinkumar Borate <Pravin103082@Mastek.Com>
2017-01-16 10:15:11 +0530

    js change

--- www/AppEngageFcm.js
+++ www/AppEngageFcm.js
@@ -1,5 +1,28 @@
 var exec = require('cordova/exec');
 
 exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "AppEngageFcm", "coolMethod", [arg0]);
+    exec(success, error, "AppEngageAnalytics", "coolMethod", [arg0]);
+};
+exports.send= function (success,error){
+    exec(success, error, "AppEngageAnalytics", "send", []);
+};
+
+exports.event= function (arg0,success,error){
+    exec(success, error, "AppEngageAnalytics", "event", arg0);
+};
+exports.startScreen= function (success,error){
+    exec(success, error, "AppEngageAnalytics", "startScreen", []);
+};
+exports.endScreen= function (arg0,success,error){
+    exec(success, error, "AppEngageAnalytics", "endScreen", [arg0]);
+};
+exports.crash= function (success,error){
+    exec(success, error, "AppEngageAnalytics", "crash", []);
+};
+exports.end= function (success,error){
+    exec(success, error, "AppEngageAnalytics", "end", []);
+};
+
+exports.my= function (success,error){
+    exec(success, error, "AppEngageAnalytics", "my", []);
 };
