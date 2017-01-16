3d926f6a07d9ecbb7b3ce09e338c353326e9ae81
Pravin <Pravinkumar.Borate@mastek.com>
2017-01-16 10:55:05 +0530

    js name changes

--- www/AppEngageFcm.js
+++ www/AppEngageFcm.js
@@ -1,28 +1,28 @@
 var exec = require('cordova/exec');
 
 exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "AppEngageAnalytics", "coolMethod", [arg0]);
+    exec(success, error, "AppEngageFcm", "coolMethod", [arg0]);
 };
 exports.send= function (success,error){
-    exec(success, error, "AppEngageAnalytics", "send", []);
+    exec(success, error, "AppEngageFcm", "send", []);
 };
 
 exports.event= function (arg0,success,error){
-    exec(success, error, "AppEngageAnalytics", "event", arg0);
+    exec(success, error, "AppEngageFcm", "event", arg0);
 };
 exports.startScreen= function (success,error){
-    exec(success, error, "AppEngageAnalytics", "startScreen", []);
+    exec(success, error, "AppEngageFcm", "startScreen", []);
 };
 exports.endScreen= function (arg0,success,error){
-    exec(success, error, "AppEngageAnalytics", "endScreen", [arg0]);
+    exec(success, error, "AppEngageFcm", "endScreen", [arg0]);
 };
 exports.crash= function (success,error){
-    exec(success, error, "AppEngageAnalytics", "crash", []);
+    exec(success, error, "AppEngageFcm", "crash", []);
 };
 exports.end= function (success,error){
-    exec(success, error, "AppEngageAnalytics", "end", []);
+    exec(success, error, "AppEngageFcm", "end", []);
 };
 
 exports.my= function (success,error){
-    exec(success, error, "AppEngageAnalytics", "my", []);
+    exec(success, error, "AppEngageFcm", "my", []);
 };
